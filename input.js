8bfa432c43304d8bf6b330fd3d54107ef096f1e6
karlyabut <karleisenyabut@gmail.com>
2019-08-28 17:52:47 +0000

    implemented controlable snake

--- input.js
+++ input.js
@@ -1,20 +1,40 @@
-const setupInput = function(data, callback) {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
-  stdin.on('data', (key) => {
-    if(key === '\u0003') {
-      handleUserInput(key);
-    }
-  });
+  console.log(connection);
+  handleUserInput();
 
   return stdin;
 }
 
 const handleUserInput = function() {
-  process.exit();
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    console.log(key);
+    if(key === '\u0003') {
+      // console.log("I DID IT!");
+      process.exit();
+    }
+    if(key === "w") {
+      console.log("Moving up");
+      connection.write("Move: up");
+    } else if (key === "s") {
+      console.log("moving down");
+      connection.write("Move: down");
+    } else if (key === "a") {
+      console.log("moving left");
+      connection.write("Move: left");
+    } else if (key === "d") {
+      console.log("moving right");
+      connection.write("Move: right");
+    }
+  });
 }
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -2,29 +2,5 @@
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function(data, callback) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', (key) => {
-    if(key === '\u0003') {
-      handleUserInput(key);
-    }
-  });
-
-  return stdin;
-}
-
-const handleUserInput = function() {
-  process.exit();
-}
-
 console.log('Connecting ...');
-connect();
-setupInput();
+setupInput(connect());
